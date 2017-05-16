aad54f884a0b1c141eb5384d8d0fb4208be3327d
Kylie Rose <thisiskylierose@gmail.com>
2017-05-16 21:29:54 +1000

    Updated async/await pattern and removed bluebird

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   target: 'node',
   entry: [
-    'babel-polyfill',
+    'babel-regenerator-runtime',
     path.join(__dirname, 'src/handler.js'),
   ],
   output: {
